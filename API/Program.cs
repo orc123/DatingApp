0764f0dadcce68fc9668d777dea6dce1d5c94624
Dong Pham <dong.orc12@gmail.com>
2025-07-27 09:26:03 +0700

    End Session 3

--- API/Program.cs
+++ API/Program.cs
@@ -5,16 +5,25 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddControllers();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-builder.Services.AddControllers();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors(policy =>
+{
+    policy.AllowAnyHeader()
+          .AllowAnyMethod()
+          .WithOrigins("http://localhost:4200", "https://localhost:4200");
+});
 
 app.MapControllers();
 
